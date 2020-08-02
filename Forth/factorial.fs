411fdf22c83d5670407671d5d9906fc50082c716
Christophe Thibaut <cthibaut@octo.com>
2020-08-02 17:35:45 +0200

    refactoring

--- Forth/factorial.fs
+++ Forth/factorial.fs
@@ -1,42 +1,22 @@
+\ given N, how many trailing zeroes in N! ?
+\ i.e how many multiplication 2*5 in N
+: Z ( N -- m )
+    0 SWAP 1         \ m,N,d
+    BEGIN   
+        5 *          \ 
+        OVER OVER /  \ m,N,d,q
+        >R ROT R@ +  \ N,d,m'
+        -ROT R> 0=   \ m,N,d,f 
+    UNTIL DROP DROP ;
 
-
-: NB-MULT  ( N,C -- m )
-    2>R                 \ keep N and C on return stack
-    0 1 BEGIN           \ accumulator, divisor on stack
-       2R@              \ get a copy of N and C
-       ROT * DUP        \ m, N, dC, dC
-       -ROT /           \ m, dC, N/dC
-       ROT OVER +       \ dC, N/dC, m+N/dC
-       -ROT             \ m+N/dC, dC, N/dC
-    0= UNTIL            \ new acc and new divison on stack
-    DROP 2R> 2DROP ;    \ get rid of all except result
-
-\ number of trailing zeros in a factorial
-\ this number is the number of times a 
-\ multiple of 5 is multiplied with a 
-\ multiple of 2
-\ e.g for 100
-\ nb of multiplications by 5 = 100/5 + 100/25 + 100/125 ... = 24
-\ nb of multiplications by 2 = 100/2 + 100/4  + 100/8   ... = 97
-\ nb of times 2 is multiplied by 5 = 24
-
-: Z ( N -- Z )
-    DUP  5 NB-MULT 
-    SWAP 2 NB-MULT
-    MIN ;
-
-\ read a line on stdin, assume no exception
-: READLN ( -- addr,l )
-    PAD DUP 40 
-    STDIN READ-LINE THROW
-    DROP ;
+\ read a number on stdin, assume no exception
+: READ-INT ( -- addr,l )
+    PAD DUP 40 STDIN READ-LINE THROW DROP 
+    S>NUMBER? DROP DROP ;
 
 : MAIN
-    READLN EVALUATE
-    0 DO
-        READLN EVALUATE
-         Z . CR
+    READ-INT 0 DO 
+        READ-INT Z . CR 
     LOOP ;
 
-MAIN
-BYE
+MAIN BYE
